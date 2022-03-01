e82896ca34303261389fa3e4bc0cea8c0e7d5fb5
Abhishek-90 <holaniabhishek90@gmail.com>
2022-03-01 18:40:15 +0530

    Started to Implemented hook to fetch tracks

--- src/components/FilterBar/TrackFilter.tsx
+++ src/components/FilterBar/TrackFilter.tsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Select from "react-select";
 
+interface IOption {
+  value: string;
+  label: string;
+}
+
 const TrackFilter = () => {
+
+  const options:IOption[] = [];
+
+  useEffect(() => {
+    // Implement hook to fetch tracks from Track-Api on Page load.
+  },[])
   return (
     <div className="track-filter">
-      <Select className="bg-filters-color h-full w-full pl-4 font-Poppins text-new-old rounded-lg" />
+      <Select 
+        className="bg-filters-color h-full w-full pl-4 font-Poppins text-new-old rounded-lg" 
+        options={options}
+      />
     </div>
   );
 };
