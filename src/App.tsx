5b39948b91227149a9e807a03f2b8f1ca798cb9a
Abhishek-90 <holaniabhishek90@gmail.com>
2022-03-03 17:08:01 +0530

    Added Dashboard, tracks, Mentors and Contribute link to Navigation Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import './App.css';
-import NavigationBar from './components/NavigationBar/NavigationBar';
-import { TestimonialsList } from './components/TestimonialsList/TestimonialsList';
+import React from "react";
+import "./App.css";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
+import { TestimonialsList } from "./components/TestimonialsList/TestimonialsList";
 
 function App() {
   return (
     <>
-      <NavigationBar/>
-    <div className="flex justify-center">
-      <TestimonialsList/>
-    </div>
+      <NavigationBar />
+      <div className="flex justify-center">
+        <TestimonialsList />
+      </div>
     </>
   );
 }
--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const NavigationBar = () => {
   return (
-    <div className="navigation w-full h-16 flex flex-row border-b-2 border-solid border-navigation-border">
+    <div className="navigation w-full h-16 flex flex-row border-b-2 border-solid border-navigation-border space-x-6">
       <div className="exercism-logo ml-8 grid items-center">
-      <a href="/"><img src="https://d24y9kuxp2d7l2.cloudfront.net/assets/icons/exercism-with-logo-black-b427c06c6a068ba9f391734115e4d22dfa876d1d.svg" alt="exercism-logo"/></a>
+        <a href="/">
+          <img
+            src="https://d24y9kuxp2d7l2.cloudfront.net/assets/icons/exercism-with-logo-black-b427c06c6a068ba9f391734115e4d22dfa876d1d.svg"
+            alt="exercism-logo"
+          />
+        </a>
+      </div>
+      <div className="dashboard-link grid items-center">
+        <a href="/">
+          <p className="font-Poppins font-semibold">Dashboard</p>
+        </a>
+      </div>
+      <div className="dashboard-link grid items-center">
+        <a href="/">
+          <p className="font-Poppins font-semibold">Tracks</p>
+        </a>
+      </div>
+      <div className="dashboard-link grid items-center">
+        <a href="/">
+          <p className="font-Poppins font-semibold">Mentoring</p>
+        </a>
+      </div>
+      <div className="dashboard-link grid items-center">
+        <a href="/">
+          <p className="font-Poppins font-semibold">Contribute</p>
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
