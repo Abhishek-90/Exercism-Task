6196f7c1f6ee13ce1b56592551afddce6139d3a3
Abhishek-90 <holaniabhishek90@gmail.com>
2022-03-04 18:47:45 +0530

    Styling Completed : Pagination Part Pending

--- src/components/FilterBar/TrackFilter.tsx
+++ src/components/FilterBar/TrackFilter.tsx
@@ -1,56 +1,68 @@
 import axios from "axios";
 import React, { useEffect } from "react";
 import Select from "react-select";
-import { tracksEndpoint } from '../../constants/ApiEndpoint'
+import { tracksEndpoint } from "../../constants/ApiEndpoint";
 interface IOption {
   value: string;
   label: JSX.Element;
 }
 
 const fetchTracks = async () => {
-  const response = await axios.get(tracksEndpoint)
-  return response.data.tracks
-}
+  const response = await axios.get(tracksEndpoint);
+  return response.data.tracks;
+};
 
 const TrackFilter = () => {
-
-  const options:IOption[] = [];
+  const options: IOption[] = [];
 
   useEffect(() => {
-
     fetchTracks().then((response) => {
-      response.forEach((item:any)=> {
+      response.forEach((item: any) => {
         const temp = {
           value: item.title,
-          label: <div><img className="h-12 p-0" src={item.icon_url} alt={item.title}/></div>
-        }
-        options.push(temp)
-      })
-    })
-  },)
+          label: (
+            <div>
+              <img className="h-12 p-0" src={item.icon_url} alt={item.title} />
+            </div>
+          ),
+        };
+        options.push(temp);
+      });
+    });
+  });
 
   return (
     <div className="track-filter h-12 ml-4">
-      <Select 
+      <Select
         isMulti={false}
         isSearchable={false}
         options={options}
         defaultValue={{
-          value: '',
-          label: <div><img className="h-12 p-0" src="https://d24y9kuxp2d7l2.cloudfront.net/assets/icons/logo-42e9b829cf6816496069a62608cb51e7c13624bd.svg" alt=""/></div>
+          value: "",
+          label: (
+            <div 
+              className="grid items-center h-12 w-x-0"
+            >
+              <img
+                className="h-8 p-0"
+                src="https://d24y9kuxp2d7l2.cloudfront.net/assets/icons/logo-42e9b829cf6816496069a62608cb51e7c13624bd.svg"
+                alt="Symbol"
+              />
+            </div>
+          ),
         }}
         className="my-4"
         components={{
-          IndicatorSeparator: ()=>null
+          IndicatorSeparator: () => null,
         }}
         styles={{
-          control: base => ({
+          control: (base) => ({
             ...base,
             border: 0,
-            outline: 0
-          })
+            outline: 0,
+          }),
         }}
-        onChange= {(value)=> console.log(value)}
+        onChange={(value) => console.log(value)}
       />
     </div>
   );
