a2a84aa06c88cd50594187c8029bf807eb859b19
Abhishek-90 <holaniabhishek90@gmail.com>
2022-03-04 17:42:03 +0530

    Changed Cursor

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -15,17 +15,17 @@ const NavigationBar = () => {
         <div className="dashboard-link grid items-center ml-2">
           <a className="flex flex-row space-x-2 ml-4" href="/">
             <img
-              className="h-6 w-6"
+              className="h-6 w-6 "
               src="https://d24y9kuxp2d7l2.cloudfront.net/assets/icons/dashboard-d6d946e5b42a4aef577066eca7b695641e21130a.svg"
               alt="mentor Logo"
             />
             <p className="font-Poppins font-semibold">Dashboard</p>
           </a>
         </div>
-        <div className="dashboard-link grid items-center">
+        <div className="dashboard-link grid items-center ">
           <a className="flex flex-row space-x-2 ml-4" href="/">
             <img
-              className="h-6 w-6"
+              className="h-6 w-6 hue-rotate-60"
               src="https://d24y9kuxp2d7l2.cloudfront.net/assets/icons/tracks-2e780b460e113a9b07ce4446c988a31c40547b00.svg"
               alt="mentor Logo"
             />
@@ -63,43 +63,43 @@ const NavigationBar = () => {
       {/* {----------------------------------------------------------------------------------------------------------------------------} */}
       
       <div className="right-size buttons flex flex-row space-x-8">
-        <div className="random-picture  grid items-center ml-64">
+        <div className="random-picture  grid items-center ml-64 cursor-pointer">
           <img
             className="h-6 w-8 rounded-full"
             src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzEiIGhlaWdodD0iMjciIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTExIDQuNzMyYTIgMiAwIDAgMSAyIDBMMjAuMzkyIDlhMiAyIDAgMCAxIDEgMS43MzJ2OC41MzZhMiAyIDAgMCAxLTEgMS43MzJMMTMgMjUuMjY4YTIgMiAwIDAgMS0yIDBMMy42MDggMjFhMiAyIDAgMCAxLTEtMS43MzJ2LTguNTM2YTIgMiAwIDAgMSAxLTEuNzMyTDExIDQuNzMyWiIgZmlsbD0iI2ZmZiIgc3Ryb2tlPSJ1cmwoI2EpIiBzdHJva2Utd2lkdGg9IjIiLz48Y2lyY2xlIGN4PSIyNCIgY3k9IjciIHI9IjUuNSIgZmlsbD0iI0VCNTc1NyIgc3Ryb2tlPSIjZmZmIiBzdHJva2Utd2lkdGg9IjMiLz48ZGVmcz48bGluZWFyR3JhZGllbnQgaWQ9ImEiIHgxPSIxMiIgeTE9IjMiIHgyPSIxMiIgeTI9IjI3IiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+PHN0b3Agc3RvcC1jb2xvcj0iIzIwRiIvPjxzdG9wIG9mZnNldD0iMSIgc3RvcC1jb2xvcj0iIzlFMDBGRiIvPjwvbGluZWFyR3JhZGllbnQ+PC9kZWZzPjwvc3ZnPg=="
             alt="random"
           />
         </div>
-        <div className="random-picture  grid items-center">
+        <div className="random-picture  grid items-center cursor-pointer">
           <img
             className="h-6 w-8 rounded-full"
             src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzEiIGhlaWdodD0iMjciIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHBhdGggZD0iTTExIDQuNzMyYTIgMiAwIDAgMSAyIDBMMjAuMzkyIDlhMiAyIDAgMCAxIDEgMS43MzJ2OC41MzZhMiAyIDAgMCAxLTEgMS43MzJMMTMgMjUuMjY4YTIgMiAwIDAgMS0yIDBMMy42MDggMjFhMiAyIDAgMCAxLTEtMS43MzJ2LTguNTM2YTIgMiAwIDAgMSAxLTEuNzMyTDExIDQuNzMyWiIgZmlsbD0iI2ZmZiIgc3Ryb2tlPSJ1cmwoI2EpIiBzdHJva2Utd2lkdGg9IjIiLz48Y2lyY2xlIGN4PSIyNCIgY3k9IjciIHI9IjUuNSIgZmlsbD0iI0VCNTc1NyIgc3Ryb2tlPSIjZmZmIiBzdHJva2Utd2lkdGg9IjMiLz48ZGVmcz48bGluZWFyR3JhZGllbnQgaWQ9ImEiIHgxPSIxMiIgeTE9IjMiIHgyPSIxMiIgeTI9IjI3IiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSI+PHN0b3Agc3RvcC1jb2xvcj0iIzIwRiIvPjxzdG9wIG9mZnNldD0iMSIgc3RvcC1jb2xvcj0iIzlFMDBGRiIvPjwvbGluZWFyR3JhZGllbnQ+PC9kZWZzPjwvc3ZnPg=="
             alt="random"
           />
         </div>
-        <div className="notification-picture h-8 w-8 my-4  shadow drop-shadow-2xl grid items-center">
+        <div className="notification-picture h-8 w-8 my-4  shadow drop-shadow-2xl grid items-center cursor-pointer">
           <img
             className="h-6 w-8"
             src="https://d24y9kuxp2d7l2.cloudfront.net/icons/notifications-2e6c2d672e24a543905250aefc304f77e88718ff.svg"
             alt="Notification"
           />
         </div>
-        <div className="reputation-picture w-24 flex flex-row content-center my-4 rounded-r-full rounded-l-full ring-8 ring-violet-700 bg-black">
+        <div className="reputation-picture w-24 flex flex-row content-center my-4 rounded-r-full rounded-l-full ring-8 ring-violet-700 bg-black cursor-pointer">
           <img
-            className="h-6 w-6 ml-3 mt-1 filter invert sepia hue-rotate-236 brightness-100 contrast-100 saturate-1618"
+            className="h-6 w-6 ml-3 mt-1 filter invert"
             src="https://d24y9kuxp2d7l2.cloudfront.net/icons/reputation-5b5938e36519908ac61075db3b9826307a0f907a.svg"
             alt="reputation"
           />
           <p className="font-Poppins ml-1 mt-1 text-white">300K</p>
         </div>
-        <div className="profile-picture  grid items-center">
+        <div className="profile-picture  grid items-center cursor-pointer">
           <img
             className="h-12 w-12 rounded-full"
             src="https://avatars.githubusercontent.com/u/43419831?v=4"
             alt="profile"
           />
         </div>
-        <div className="menu-button grid items-center">
+        <div className="menu-button grid items-center cursor-pointer">
           <img
             className="h-6 w-4"
             src="https://d24y9kuxp2d7l2.cloudfront.net/assets/icons/more-vertical-371ef6f2314bb5dbe5d3892a7ee098c6ebc3cf30.svg"
