f137d23131c35a841a4aa190023ee4226e647b04
Abhishek-90 <holaniabhishek90@gmail.com>
2022-03-07 13:45:01 +0530

    Added Static Component about TestimonialsList

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import {NavigationBar} from "./components/NavigationBar/NavigationBar";
+import { StaticContent } from "./components/StaticComponent/StaticContent";
 import { TestimonialsList } from "./components/TestimonialsList/TestimonialsList";
 
 function App() {
   return (
     <>
       <NavigationBar />
+      <StaticContent/>
       <div className="flex justify-center">
         <TestimonialsList />
       </div>
