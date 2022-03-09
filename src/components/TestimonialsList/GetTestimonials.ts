a7cfa1e0ea048125c98ed9bb2959b37e67661820
Abhishek-90 <holaniabhishek90@gmail.com>
2022-03-09 20:06:51 +0530

    GetTestimonials function Code Refactor

--- src/components/TestimonialsList/GetTestimonials.ts
+++ src/components/TestimonialsList/GetTestimonials.ts
@@ -2,7 +2,6 @@ import axios, { AxiosResponse } from "axios";
 import { testimonials } from "../../constants/ApiEndpoint";
 
 export const getTestimonials = async (page?:string, order?:string, track?:string, exercise?:string) => {
-  console.log(`${testimonials}?page=${page}&order=${order}${track ? `&track=${track}`:``}${exercise ? `&exercise=${exercise}`:``}`)
   try {
     const res: AxiosResponse<any> = await axios.get(
       `${testimonials}?page=${page}&order=${order}${track ? `&track=${track}`:``}${exercise ? `&exercise=${exercise}`:``}`
@@ -12,5 +11,4 @@ export const getTestimonials = async (page?:string, order?:string, track?:string
   } catch (error) {
     console.error(error)
   }
-  
 };
