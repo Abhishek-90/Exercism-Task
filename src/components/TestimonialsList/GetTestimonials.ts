36de984177dfb9518414eaf65848a7f3b8ce70d2
Abhishek-90 <holaniabhishek90@gmail.com>
2022-03-22 18:21:42 +0530

    Implemented Algorithm to display only 10 Testimonials

--- src/components/TestimonialsList/GetTestimonials.ts
+++ src/components/TestimonialsList/GetTestimonials.ts
@@ -1,10 +1,11 @@
 import axios, { AxiosResponse } from "axios";
 import { testimonials } from "../../constants/ApiEndpoint";
 
-export const getTestimonials = async (page?:string, order?:string, track?:string, exercise?:string) => {
+export const getTestimonials = async (page:number, order?:string, track?:string, exercise?:string) => {
+  const changedPage:number = (page/2) + (page%2) 
   try {
     const res: AxiosResponse<any> = await axios.get(
-      `${testimonials}?page=${page}&order=${order}${track ? `&track=${track}`:``}${exercise ? `&exercise=${exercise}`:``}`
+      `${testimonials}?page=${changedPage}&order=${order}${track ? `&track=${track}`:``}${exercise ? `&exercise=${exercise}`:``}`
     ) 
 
     return res.data  
--- src/components/TestimonialsList/TestimonialsList.tsx
+++ src/components/TestimonialsList/TestimonialsList.tsx
@@ -12,6 +12,14 @@ const doublePages = (totalPage: number) : number => {
   return (totalPage/10) + (totalPage%10 === 0 ? 0 : 1)
 }
 
+const getTenTestimonials = (testimonials:ITestimonials[], page:number) : ITestimonials[] => {
+  if(page%2 === 1) {
+    return testimonials.slice(0,Math.min(10,testimonials.length))
+  }
+
+  return testimonials.slice(10,Math.min(20,testimonials.length))
+}
+
 const TestimonialsList = () => {
 
   const [testimonials, setTestimonials] = useState<ITestimonials[]>([])
@@ -21,8 +29,9 @@ const TestimonialsList = () => {
   useEffect(() => {
     const getTestimonialsCaller = async () => {
       const res = await getTestimonials(page,order,track, exercise)
+      const finalTestimonials = getTenTestimonials(res.testimonials.results,page)
       dispatch(getTotalPage(doublePages(res.testimonials.pagination.total_count)))
-      setTestimonials(res.testimonials.results)
+      setTestimonials(finalTestimonials)
     }
 
     getTestimonialsCaller()
