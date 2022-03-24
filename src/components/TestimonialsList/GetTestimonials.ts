0ba8d55a2ea64be84a6026431ecdc57ae0ef309b
Abhishek-90 <holaniabhishek90@gmail.com>
2022-03-24 23:18:53 +0530

    Pagination Code Refactor

--- src/components/TestimonialsList/GetTestimonials.ts
+++ src/components/TestimonialsList/GetTestimonials.ts
@@ -1,11 +1,10 @@
 import axios, { AxiosResponse } from "axios";
 import { testimonials } from "../../constants/ApiEndpoint";
 
-export const getTestimonials = async (page:number, order?:string, track?:string, exercise?:string) => {
-  const changedPage:number = (page/2) + (page%2) 
+export const getTestimonials = async (page:number, order?:string, track?:string, exercise?:string) => { 
   try {
     const res: AxiosResponse<any> = await axios.get(
-      `${testimonials}?page=${changedPage}&order=${order}${track ? `&track=${track}`:``}${exercise ? `&exercise=${exercise}`:``}`
+      `${testimonials}?page=${page}&order=${order}${track ? `&track=${track}`:``}${exercise ? `&exercise=${exercise}`:``}`
     ) 
 
     return res.data  
