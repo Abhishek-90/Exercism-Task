bcae6d9ab45084355636584677d42679148286ef
Abhishek-90 <holaniabhishek90@gmail.com>
2022-03-26 11:05:34 +0530

    Changed Calculate Date function

--- src/components/TestimonialsListItem/CalculateDate.ts
+++ src/components/TestimonialsListItem/CalculateDate.ts
@@ -1,6 +1,14 @@
 import { formatDistanceToNowStrict } from 'date-fns'
 
 export const getDate = (date:string) : string => {
-    const convertedDate = new Date(date);
-    return formatDistanceToNowStrict(convertedDate);
+    const convertedDate = new Date(date)
+    const formatedDate = formatDistanceToNowStrict(convertedDate)
+    const formatedDateArray = formatedDate.split(" ") 
+    if(formatedDateArray[0] !== "1") {
+        return formatedDate
+    }
+    
+    formatedDateArray[0] = formatedDateArray[1].charAt(0) === "h" ? "an" : "a"
+
+    return formatedDateArray.join(" ")
 }
