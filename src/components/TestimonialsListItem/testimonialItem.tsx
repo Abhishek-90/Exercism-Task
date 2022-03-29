50b513aece9a544b50919d66ffe75218c20059ad
Abhishek-90 <holaniabhishek90@gmail.com>
2022-03-29 18:10:53 +0530

    Removed Errors in SVG TestimonialItem

--- src/components/TestimonialsListItem/testimonialItem.tsx
+++ src/components/TestimonialsListItem/testimonialItem.tsx
@@ -64,11 +64,11 @@ const TestimonialItem = ({
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
-          stroke-width="1.5"
+          strokeWidth="1.5"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M9 5l7 7-7 7"
           />
         </svg>
