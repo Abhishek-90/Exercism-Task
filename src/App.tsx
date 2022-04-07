8cb83df2b1bf53622ea3d987a9f4f977841102fe
Abhishek-90 <holaniabhishek90@gmail.com>
2022-04-07 18:35:34 +0530

    Using Conditional rendering for loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import { Filters } from "./components/FilterBar/Filters";
 import { NavigationBar } from "./components/NavigationBar/NavigationBar";
@@ -7,6 +8,8 @@ import { StaticContent } from "./components/StaticComponent/StaticContent";
 import { TestimonialsList } from "./components/TestimonialsList/TestimonialsList";
 
 function App() {
+  const { isLoading } = useSelector((state: any) => state.testimonial)
+
   return (
     <>
       <NavigationBar />
@@ -14,7 +17,7 @@ function App() {
       <div className="flex flex-col justify-center px-4">
         <div className="testimonials-list rounded-lg shadow-3xl mt-8 mb-16">
           <Filters />
-          <TestimonialsList />
+          {isLoading ? <TestimonialsList />:<TestimonialsList />}
           <Pagination />
         </div>
       </div>
