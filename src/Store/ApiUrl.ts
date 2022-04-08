ec76c71bd93c1c9a9925ea6b41fadf61b9c0e593
Abhishek-90 <holaniabhishek90@gmail.com>
2022-04-08 12:32:44 +0530

    Loading Component working

--- src/Store/ApiUrl.ts
+++ src/Store/ApiUrl.ts
@@ -1,52 +1,59 @@
-import { createSlice } from '@reduxjs/toolkit' 
+import { createSlice } from "@reduxjs/toolkit";
 
 interface ITestimonialUrlProps {
-    order?: string,
-    page: number,
-    track?: string,
-    exercise?: string, 
-    totalPage?: number,
-    isLoading:boolean,
+  order?: string;
+  page: number;
+  track?: string;
+  exercise?: string;
+  totalPage?: number;
+  isLoading: boolean;
 }
 
 const testimonialUrlProps: ITestimonialUrlProps = {
-    order: 'newest_first',
-    page: 1,
-    totalPage:0,
-    isLoading: true,
-}
+  order: "newest_first",
+  page: 1,
+  totalPage: 0,
+  isLoading: true,
+};
 
 export const testimonialSlice = createSlice({
-    name: 'testimonialsUrl',
-    initialState:testimonialUrlProps,
-    reducers:{
-      changePageByOne: (state, action)=>{
-        state.page = state.page + action.payload
-      },
-      changeOrder: (state, action) => {
-        state.page = 1;
-        state.order = action.payload
-      },
-      getTotalPage: (state, action) => {
-        state.totalPage = action.payload
-      },
-      changeTrack: (state, action) => {
-        state.page = 1
-        state.track = action.payload
-      },
-      changePageByPayload: (state,action) => {
-        state.page = action.payload;
-      },
-      changeExercise: (state, action) => {
-        state.page = 1
-        state.exercise = action.payload
-      },
-      setIsLoading: (state, action) => {
-        state.isLoading = action.payload
-      }
-     
-    }
-})
+  name: "testimonialsUrl",
+  initialState: testimonialUrlProps,
+  reducers: {
+    changePageByOne: (state, action) => {
+      state.page = state.page + action.payload;
+    },
+    changeOrder: (state, action) => {
+      state.page = 1;
+      state.order = action.payload;
+    },
+    getTotalPage: (state, action) => {
+      state.totalPage = action.payload;
+    },
+    changeTrack: (state, action) => {
+      state.page = 1;
+      state.track = action.payload;
+    },
+    changePageByPayload: (state, action) => {
+      state.page = action.payload;
+    },
+    changeExercise: (state, action) => {
+      state.page = 1;
+      state.exercise = action.payload;
+    },
+    setIsLoading: (state, action) => {
+      state.isLoading = action.payload;
+    },
+  },
+});
 
-export const { changePageByOne, changeOrder, getTotalPage,changeTrack, changePageByPayload, changeExercise, setIsLoading } = testimonialSlice.actions
-export default testimonialSlice.reducer
+export const {
+  changePageByOne,
+  changeOrder,
+  getTotalPage,
+  changeTrack,
+  changePageByPayload,
+  changeExercise,
+  setIsLoading,
+} = testimonialSlice.actions;
+export default testimonialSlice.reducer;
--- src/components/TestimonialsList/TestimonialsList.tsx
+++ src/components/TestimonialsList/TestimonialsList.tsx
@@ -3,48 +3,48 @@ import { TestimonialItem } from "../TestimonialsListItem/testimonialItem";
 import { ITestimonials } from "../../interfaces/testimonials";
 import { getTestimonials } from "./GetTestimonials";
 import { useDispatch, useSelector } from "react-redux";
-import { getTotalPage, setIsLoading } from '../../Store/ApiUrl'
-import NoTestimonials from "../StaticComponent/NoTestimonials";
+import { getTotalPage, setIsLoading } from "../../Store/ApiUrl";
+// import NoTestimonials from "../StaticComponent/NoTestimonials";
 import Loading from "../Loading/Loading";
 
 const TestimonialsList = () => {
-
-  const [testimonials, setTestimonials] = useState<ITestimonials[]>([])
-  const { page, order, track, exercise, isLoading } = useSelector((state: any) => state.testimonial)
-  const dispatch = useDispatch()
+  const [testimonials, setTestimonials] = useState<ITestimonials[]>([]);
+  const { page, order, track, exercise, isLoading } = useSelector(
+    (state: any) => state.testimonial
+  );
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    // dispatch(setIsLoading(true))
-    console.log(isLoading)
     const getTestimonialsCaller = async () => {
-      const res = await getTestimonials(page,order,track, exercise)
-      dispatch(getTotalPage(res.testimonials.pagination.total_pages))
-      setTestimonials(res.testimonials.results)
-    }
+      dispatch(setIsLoading(true));
+      const res = await getTestimonials(page, order, track, exercise);
+      dispatch(getTotalPage(res.testimonials.pagination.total_pages));
+      setTestimonials(res.testimonials.results);
+      dispatch(setIsLoading(false));
+    };
 
-    getTestimonialsCaller()
-    // dispatch(setIsLoading(false))
-    console.log(isLoading)
-  },[page, order, track, exercise])
+    getTestimonialsCaller();
+  }, [page, order, track, exercise]);
 
   return (
-      <>
+    <>
       {isLoading && <Loading />}
-      {!isLoading && testimonials.map((item) => {
-        return (
-          <TestimonialItem
-          key={item.id}
-          track_icon_url={item.track.icon_url}
-          avatar_icon_url={item.mentor.avatar_url}
-          content={item.content}
-          reviewer_name={item.mentor.handle}
-          track_name={item.track.title}
-          date_created={item.created_at}
-          exercise_title={item.exercise.title}
-          />
+      {!isLoading &&
+        testimonials.map((item) => {
+          return (
+            <TestimonialItem
+              key={item.id}
+              track_icon_url={item.track.icon_url}
+              avatar_icon_url={item.mentor.avatar_url}
+              content={item.content}
+              reviewer_name={item.mentor.handle}
+              track_name={item.track.title}
+              date_created={item.created_at}
+              exercise_title={item.exercise.title}
+            />
           );
         })}
-      </>
+    </>
   );
 };
 
