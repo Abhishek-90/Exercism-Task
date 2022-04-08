c50122f83f99a613260f6e39347e95bd55a3baf7
Abhishek-90 <holaniabhishek90@gmail.com>
2022-04-08 14:42:47 +0530

    Added SVG Arrow to select

--- src/components/FilterBar/NewOldFilter.tsx
+++ src/components/FilterBar/NewOldFilter.tsx
@@ -4,14 +4,15 @@ import { changeOrder } from "../../Store/ApiUrl";
 
 const NewOldFilter = () => {
   const dispatch = useDispatch();
-  const filterRef:any = useRef(HTMLSelectElement)
+  const filterRef: any = useRef(HTMLSelectElement);
   return (
-      
-      <div className="new-old-filter w-80 h-full float-right my-4 mr-6 flex">
+    <div className="new-old-filter w-80 h-full float-right my-4 mr-6 flex">
       <svg
-        onClick={() => filterRef.current.click}
+        onClick={() => {
+          filterRef.current.click();
+        }}
         xmlns="http://www.w3.org/2000/svg"
-        className="h-9 w-9 mt-1.5 absolute ml-[275px] cursor-pointer"
+        className="h-9 w-9 mt-1.5 absolute ml-[275px] stroke-slate-500"
         fill="none"
         viewBox="0 0 24 24"
         stroke="currentColor"
@@ -19,18 +20,19 @@ const NewOldFilter = () => {
       >
         <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
       </svg>
-        <select
-          ref={filterRef}
-          className="bg-filters-color h-full w-full pl-4 font-Poppins text-new-old rounded-lg 
+      <select
+        ref={filterRef}
+        className="bg-filters-color h-full w-full pl-4 font-Poppins text-new-old rounded-lg 
         appearance-none"
-          onChange={(e) => {
-            dispatch(changeOrder(e.target.value));
-          }}
-        >
-          <option value="newest_first">Sort by Most Recent</option>
-          <option value="oldest_first">Sort by Least Recent</option>
-        </select>
-      </div>
+        onChange={(e) => {
+          dispatch(changeOrder(e.target.value));
+        }}
+        onClick={() => console.log("Select")}
+      >
+        <option value="newest_first">Sort by Most Recent</option>
+        <option value="oldest_first">Sort by Least Recent</option>
+      </select>
+    </div>
   );
 };
 
--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import loading from './loading.gif'
 const Loading = () => {
-  console.log("Loaded")
   return (
     <div className="w-full flex justify-center">
       <img className="" src={loading} alt="loading"/>
