52bc46213fcfca01cf348a16df051c489c9e4fe6
Abhishek-90 <holaniabhishek90@gmail.com>
2022-04-09 17:23:06 +0530

    Minor Changes in Custom Select

--- src/components/FilterBar/NewOldFilter.tsx
+++ src/components/FilterBar/NewOldFilter.tsx
@@ -6,13 +6,14 @@ const NewOldFilter = () => {
   const dispatch = useDispatch();
   const [filterValue, setFilterValue] = useState("Most")
   const [active, setActive] = useState<boolean>(false)
+  
   return (
     <div className="new-old-filter w-80 h-full float-right my-4 mr-6">
       <div className={`select-box absolute font-Poppins bg-filters-color w-80 ${active ? 'h-fit':'h-12'} py-1 px-4 text-md text-new-old rounded-lg`}>
 
         <div className="selected bg-downArrow bg-no-repeat bg-right cursor-pointer mt-2" onClick={() => {setActive(!active)}}>Sort by {filterValue} Recent</div>
 
-        <div className={`options-container mt-2 transition transition-all duration-500 ${active ? "h-fit":"h-0 hidden"}`}>
+        <div className={`options-container mt-3  ${active ? "h-fit":"h-0"} overflow-hidden rounded-lg`}>
           <div className="option p-1 hover:bg-filters-color-hover">
             <input
               onClick={() => {
