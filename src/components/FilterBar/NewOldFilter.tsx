499c77a720fa7d55c5825172c540ae0986529463
Abhishek-90 <holaniabhishek90@gmail.com>
2022-04-09 17:35:59 +0530

    Minor Change

--- src/components/FilterBar/NewOldFilter.tsx
+++ src/components/FilterBar/NewOldFilter.tsx
@@ -4,22 +4,37 @@ import { changeOrder } from "../../Store/ApiUrl";
 
 const NewOldFilter = () => {
   const dispatch = useDispatch();
-  const [filterValue, setFilterValue] = useState("Most")
-  const [active, setActive] = useState<boolean>(false)
-  
+  const [filterValue, setFilterValue] = useState("Most");
+  const [active, setActive] = useState<boolean>(false);
+
   return (
     <div className="new-old-filter w-80 h-full float-right my-4 mr-6">
-      <div className={`select-box absolute font-Poppins bg-filters-color w-80 ${active ? 'h-fit':'h-12'} py-1 px-4 text-md text-new-old rounded-lg`}>
+      <div
+        className={`select-box absolute font-Poppins bg-filters-color w-80 ${
+          active ? "h-fit" : "h-12"
+        } py-1 px-4 text-md text-new-old rounded-lg`}
+        onFocus={()=>{setActive(true)}}
+      >
+        <div
+          className="selected bg-downArrow bg-no-repeat bg-right cursor-pointer mt-2"
+          onClick={() => {
+            setActive(!active);
+          }}
+        >
+          Sort by {filterValue} Recent
+        </div>
 
-        <div className="selected bg-downArrow bg-no-repeat bg-right cursor-pointer mt-2" onClick={() => {setActive(!active)}}>Sort by {filterValue} Recent</div>
-
-        <div className={`options-container mt-3  ${active ? "h-fit":"h-0"} overflow-hidden rounded-lg`}>
+        <div
+          className={`options-container mt-3  ${
+            active ? "h-fit" : "h-0"
+          } overflow-hidden rounded-lg`}
+        >
           <div className="option p-1 hover:bg-filters-color-hover">
             <input
               onClick={() => {
-                setFilterValue("Most")
-                setActive(false)
-                dispatch(changeOrder("newest_first"))
+                setFilterValue("Most");
+                setActive(false);
+                dispatch(changeOrder("newest_first"));
               }}
               className="appearance-none"
               type="radio"
@@ -34,9 +49,9 @@ const NewOldFilter = () => {
           <div className="option p-1 hover:bg-filters-color-hover">
             <input
               onClick={() => {
-                setFilterValue("Least")
-                setActive(false)
-                dispatch(changeOrder("oldest_first"))
+                setFilterValue("Least");
+                setActive(false);
+                dispatch(changeOrder("oldest_first"));
               }}
               className="appearance-none"
               type="radio"
